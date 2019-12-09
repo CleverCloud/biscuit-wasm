f9524f208511fe44139b11214e4d876a4f4218a5
Geoffroy Couprie <geo.couprie@gmail.com>
2019-12-09 14:52:02 +0100

    more ui

--- example/src/index.js
+++ example/src/index.js
@@ -4,6 +4,18 @@ const fromHex = hexString =>
 const toHex = bytes =>
   bytes.reduce((str, byte) => str + byte.toString(16).padStart(2, '0'), '');
 
+const newBlock = index => {
+  var li = document.createElement("li");
+  li.innerHTML = "<li><div id=\"block_"+index+"\">" +
+    "Block n°"+index+
+    "<div id=\"block_"+index+"_serialized\" width=\"100\" height=\"300\"></div>" +
+
+    "<input type=\"button\" id=\"block_"+index+"_build\" value=\"Build\"></input>" +
+    "<div id=\"block_"+index+"_buttons\"></div>" +
+    "</div></li>";
+  return li;
+}
+
 (async function () {
 
   const biscuit = await import('biscuit-wasm');
@@ -11,8 +23,12 @@ const toHex = bytes =>
 
   const privkeyI = document.getElementById('private_key');
   const pubkeyI = document.getElementById('public_key');
+  const serializedI = document.getElementById('serialized_token');
   const genkeyB = document.getElementById('generate_keys');
   const newtokenB = document.getElementById('new_token');
+  const verifyB = document.getElementById('verify');
+  const workD = document.getElementById('work');
+  const blocksU = document.getElementById('blocks');
 
   genkeyB.addEventListener("click", () => {
     let privkey = new Uint8Array(32);
@@ -26,12 +42,70 @@ const toHex = bytes =>
 
     privkeyI.value = toHex(privkey);
     pubkeyI.value = toHex(pubkey);
-
   })
+
+  newtokenB.addEventListener("click", () => {
+    let builder = new biscuit.Biscuit()
+    let firstBlock = newBlock(0);
+    blocksU.appendChild(firstBlock);
+    const block0Build = document.getElementById('block_0_build');
+    const tokenContent = document.getElementById('token_content');
+
+    block0Build.addEventListener("click", () => {
+      let decoded = fromHex(privkeyI.value);
+      let keypair = biscuit.KeyPair.fromBytes(decoded);
+
+      let token = builder.build(keypair);
+      console.log(token);
+      let serialized = token.toVec();
+      console.log(serialized);
+      const block0Serialized = document.getElementById('block_0_serialized');
+      let b64 = btoa(String.fromCharCode(...serialized));
+      block0Serialized.innerText = "Serialized ("+serialized.length+" bytes -> "+b64.length+" in base64): "+b64;
+      console.log("b64 length"+b64.length);
+      serializedI.value = b64;
+
+      tokenContent.innerText = token.print();
+      console.log("hello")
+    });
+
+    let decoded = fromHex(privkeyI.value);
+    let keypair = biscuit.KeyPair.fromBytes(decoded);
+    tokenContent.innerText = builder.print(keypair);
+
+
+    console.log("done");
+  });
+
+  verifyB.addEventListener("click", () => {
+    let data = new Uint8Array(atob(serializedI.value).split("").map(function(c) {
+          return c.charCodeAt(0); }));
+    let token = biscuit.Biscuit.from(data);
+
+    const resourceI = document.getElementById('resource');
+    const operationI = document.getElementById('operation');
+
+    let verifier = new biscuit.Verifier()
+    verifier.addResource(resourceI.value);
+    verifier.addOperation(operationI.value);
+
+    let decoded = fromHex(privkeyI.value);
+    let k = biscuit.KeyPair.fromBytes(decoded);
+
+    const resI = document.getElementById('verification_result');
+    try {
+      let result = verifier.verify(k.publicKey(), token);
+      resI.innerText = "OK";
+    } catch(error) {
+      resI.innerText = error;
+    }
+  });
+
   let decoded = fromHex(privkeyI.value);
   let k = biscuit.KeyPair.fromBytes(decoded);
   console.log(k);
 
+/*
   let keypair = new biscuit.KeyPair()
   let public_key = keypair.publicKey()
 
@@ -81,6 +155,7 @@ const toHex = bytes =>
   verifier.addAuthorityCaveat(rule)
 
   verifier.verify(public_key, token2)
+*/
 
 /*
 	const input = document.getElementById('input');
