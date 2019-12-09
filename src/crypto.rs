3effae2c56a55955e5438f37760d525e53884251
Geoffroy Couprie <geo.couprie@gmail.com>
2019-12-09 11:17:07 +0100

    helper methods for crypto

--- src/crypto.rs
+++ src/crypto.rs
@@ -18,9 +18,36 @@ impl KeyPair {
   pub fn public_key(&self) -> PublicKey {
       PublicKey(self.0.public())
   }
+
+  #[wasm_bindgen(js_name=fromBytes)]
+  pub fn from_bytes(slice: &[u8]) -> Result<KeyPair, JsValue> {
+    let mut data = [0u8; 32];
+    if slice.len() != 32 {
+      panic!("invalid key");
+    }
+
+   data.copy_from_slice(slice);
+    if let Some(key) = crypto::PrivateKey::from_bytes(data) {
+      Ok(KeyPair(crypto::KeyPair::from(key)))
+    } else {
+      panic!("invalid key");
+    }
+  }
+
+  #[wasm_bindgen(js_name=toBytes)]
+  pub fn to_bytes(&self, slice: &mut[u8]) {
+    slice.copy_from_slice(&self.0.private().to_bytes())
+  }
 }
 
 
 #[wasm_bindgen]
 pub struct PublicKey(pub(crate) crypto::PublicKey);
 
+#[wasm_bindgen]
+impl PublicKey {
+  #[wasm_bindgen(js_name=toBytes)]
+  pub fn to_bytes(&self, slice: &mut[u8]) {
+    slice.copy_from_slice(&self.0.to_bytes())
+  }
+}
